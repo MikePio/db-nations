57bbaab850ece4bec7ec8a298f6eccf8a987bfe2
MikePio <michelepiopilla@gmail.com>
2023-09-28 18:53:13 +0200

    Completed Milestone 1

---
+++ README.md
@@ -0,0 +1,34 @@
+## Todo
+Importare il seguente database:
+`dump-nations`
+
+### DB
+![Alt text](image.png)
+
+![Alt text](image-2.png)
+
+Dopo aver creato un progetto `Maven` e aver testato la possibilità di connettersi con il database attraverso il driver **JDBC** (vedi [[2023-09-28 - JDBC|JDBC]]), eseguire le seguenti *milestones*
+
+### Milestone 1
+Scrivere una query SQL in `DBEaver` che restituisca la lista di tutte le nazioni mostrando `nome`, `id`, `nome della regione` e `nome del continente`, ordinata per nome della nazione
+
+### Milestone 2
+Nel progetto creare un programma che esegua la query della [[#Milestone 1]] e stampi a video il risultato
+
+### Milestone 3
+Modificare il programma precedente per fare in modo che un utente
+possa inserire una ricerca e filtrare i risultati:
+- chiedere all’utente di inserire una *stringa di ricerca da terminale*
+- usare quella stringa come *parametro aggiuntivo della query* in modo che i risultati vengano filtrati con un `contains` (ad esempio se l’utente cerca per `ita`, il risultato della query conterrà sia `Italy` che `Mauritania`)
+
+### Milestone 4 - Bonus
+Dopo aver stampato a video l’elenco delle `country`, chiedere all’utente di inserire l’`id` di una delle country.
+
+Sulla base di quell’`id` eseguire ulteriori ricerche su *database*, che
+restituiscano:
+- **tutte le lingue parlate** in quella country
+- **le statistiche più recenti** per quella country
+
+Stampare a video i risultati. Di seguito un esempio di programma completo:
+
+![Alt text](image-1.png)
--- src/main/java/org/java/db/nations/Main.java
+++ src/main/java/org/java/db/nations/Main.java
@@ -2,25 +2,64 @@ package org.java.db.nations;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class Main {
 
 	public static void main(String[] args) {
-																 // sostituire 3306/nome_db con la porta da collegare e con il nome del database
+		// sostituire 3306/nome_db con la porta da collegare e con il nome del database
 		final String url = "jdbc:mysql://localhost:3306/dump_nations";
 		// sostituire 'root' con il nome inserito in mamp o PHP Launcher
 		final String user = "root";
 		// sostituire '' con la password inserita in mamp o PHP Launcher
 		final String password = "";
-		
+
+		// Query in SQL
+		final String sql = " SELECT c.name, c.country_id, r.name, c2.name"
+		+ " FROM countries c" 
+		+ " JOIN regions r "
+		+ " ON c.region_id = r.region_id "
+		+ " JOIN continents c2 "
+		+ " ON r.continent_id = c2.continent_id"
+		+ " ORDER BY c.name ASC;";
+
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
-			
-			System.out.println("Connessione stabilita correttamente");
+
+			try {
+				PreparedStatement ps = conn.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) {
+					// colonna 1
+					String countryName = rs.getString(1); 
+					// colonna 2
+					int countryId = rs.getInt(2);
+					// colonna 3
+					String regionName = rs.getString(3);
+					// colonna 4
+					String continentName = rs.getString(4);
+
+					// stamapare nel terminale la query
+					System.out.println("Nazione: " + countryName 
+					+ "\nID Nazione: " + countryId 
+					+ "\nRegione: " + regionName 
+					+ "\nContinente: " + continentName);
+
+					System.out.println("\n----------------------------------\n");
+				}
+
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+
 		} catch (Exception e) {
-			
-			System.out.println("Errore di connessione: " + e.getMessage());
+
+			System.out.println("Errore: " + e.getMessage());
 		}
-		
+
 		System.out.println("\n----------------------------------\n");
 		System.out.println("The end");
 	}
